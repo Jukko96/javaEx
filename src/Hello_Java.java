082d303e2c4cae47b5cb61cd6ad76a1bfc10dca8
Jukko96 <cslzh4433@gmail.com>
2025-04-06 17:45:24 +0900

    java casting + scope test

--- src/Hello_Java.java
+++ src/Hello_Java.java
@@ -1,5 +1,8 @@
 public class Hello_Java {
     public static void main (String[] args){
         System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+
     }
 }
