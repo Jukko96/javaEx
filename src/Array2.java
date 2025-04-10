8f75d80b1f3597952bf7f0cad3b539466fc7ef35
Jukko96 <cslzh4433@gmail.com>
2025-04-10 22:43:41 +0900

    add 2dArray init

--- src/Array2.java
+++ src/Array2.java
@@ -7,7 +7,21 @@ public class Array2 {
                 d2Array[row][column] = ""+row+column;
                 System.out.println("position" + d2Array[row][column]);
             }
-
+        }
+        int [][] initArray = {{1,2,3},{4,5,6}};// 2차원 배열 초기화;
+        int [][] initArray2 = new int[][]{{1,2,3},{4,5,6}};
+        int [][] fillArray = new int[2][3];
+        int i = 1;
+        for (int row = 0; row < d2Array.length; row++) {
+            for (int column = 0; column < d2Array[row].length; column++) {
+                fillArray[row][column] = i++;
+            }
+        }
+        for (int row = 0; row < d2Array.length; row++) {
+            for (int column = 0; column < d2Array[row].length; column++) {
+                System.out.print(fillArray[row][column]+" ");
+            }
+            System.out.println();
         }
     }
 }
