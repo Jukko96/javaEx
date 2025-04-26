5acd0df104c8df361158a79ed61b60619890280c
Jukko96 <cslzh4433@gmail.com>
2025-04-26 16:10:58 +0900

    calc rectangle oop test

---
+++ src/test/Rectangle.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Rectangle {
+    int width;
+    int height;
+
+    int calculateArea(int width,int height){
+        return width * height;
+    }
+    int calculatePerimeter(int width,int height){
+        return (width + height) * 2;
+    }
+    boolean isSquare(int width,int height){
+        return width == height;
+    }
+}
