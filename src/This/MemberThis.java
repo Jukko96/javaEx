b24bf63b861ae4b4c6d03dfb99466bf182d234e7
Jukko96 <cslzh4433@gmail.com>
2025-04-28 22:02:26 +0900

    this

---
+++ src/This/MemberThis.java
@@ -0,0 +1,12 @@
+package This;
+
+public class MemberThis {
+    String memberFiled;
+    String name;
+    void initFiled(String nameParameter){
+        memberFiled = nameParameter;
+    }
+    void initName(String name){
+        this.name = name;
+    }
+}
