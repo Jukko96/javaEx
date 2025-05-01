a1b23cf65c4087acf1e1daae4cc668b53c7ecb71
Jukko96 <cslzh4433@gmail.com>
2025-05-01 16:48:05 +0900

    constructor overloading

--- src/Constructor/MemberConstructor.java
+++ src/Constructor/MemberConstructor.java
@@ -4,10 +4,17 @@ public class MemberConstructor {
     String name;
     int age;
     int grade;
+
+    MemberConstructor(String name, int age){
+        this.name = name;
+        this.age = age;
+        this.grade = 50;
+        System.out.println("생성자 실행 이름: "+ this.name+" 나이:" +this.age + " 점수: "+this.grade);
+    }
     MemberConstructor(String name, int age, int grade){
         this.name = name;
         this.age = age;
         this.grade = grade;
-        System.out.println("생성자 실행 이름: "+ name+" 나이:" +age + " 점수: "+age);
+        System.out.println("생성자 실행 이름: "+ this.name+" 나이:" +this.age + " 점수: "+this.grade);
     }
 }
