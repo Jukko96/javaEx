95f3f83a842f8868d3cf5d56ff47ee0fe883583e
Jukko96 <cslzh4433@gmail.com>
2025-05-02 08:07:33 +0900

    constructor overloading 2

---
+++ src/access/Speaker.java
@@ -0,0 +1,23 @@
+package access;
+
+public class Speaker {
+    private int volume;
+    Speaker(int volume){
+        this.volume = volume;
+    }
+    void volumeUp(){
+        if(this.volume>=100){
+            System.out.println("볼륨은 100을 넘을 수 없습니다.");
+        }else{
+            this.volume += 10;
+            System.out.println("음량을 10 증가합니다.");
+        }
+    }
+    void volumeDown(){
+        this.volume -= 10;
+        System.out.println("volumeDown 호출");
+    }
+    void showVolume(){
+        System.out.println("현재 음량: "+this.volume);
+    }
+}
