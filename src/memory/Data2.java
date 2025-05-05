eb4232561e5c5ae38cf13b255f8f10673dfd1445
Jukko96 <cslzh4433@gmail.com>
2025-05-05 16:51:16 +0900

    static keyword test

---
+++ src/memory/Data2.java
@@ -0,0 +1,12 @@
+package memory;
+
+public class Data2 {
+    public String name;
+    public static int count;
+
+    public Data2(String name){
+        this.name = name;
+        Data2.count ++;
+
+    }
+}
