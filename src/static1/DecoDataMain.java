a6f46aff9966c29e34e1e1d65cc165e78a94290f
Jukko96 <cslzh4433@gmail.com>
2025-05-07 22:29:36 +0900

    static method

--- src/static1/DecoDataMain.java
+++ src/static1/DecoDataMain.java
@@ -1,9 +1,11 @@
 package static1;
 
+import static static1.DecoData.*; //import static 을 사용해서 클래스 메소드 호출 가능
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적호출");
-        DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData decoData1 = new DecoData();
@@ -13,5 +15,8 @@ public class DecoDataMain {
         DecoData decoData2 = new DecoData();
         decoData2.instanceCall();
 
+        DecoData decoData = new DecoData();
+//        decoData.staticCall(); //실행은 가능하지만 시맨틱적을 좋지 않음
+        staticCall();
     }
 }
