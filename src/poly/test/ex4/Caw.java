5d4889ec9340f8f308b1c70ca8f781be6e3f70bf
Jukko96 <cslzh4433@gmail.com>
2025-05-17 16:34:56 +0900

    interface

---
+++ src/poly/test/ex4/Caw.java
@@ -0,0 +1,13 @@
+package poly.test.ex4;
+
+public class Caw implements InterfaceAnimal{
+    @Override
+    public void sound() {
+        System.out.println("음메");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("소 이동");
+    }
+}
