3f93a0a62bc26bde93b15b5637c51d78a12af81e
Jukko96 <cslzh4433@gmail.com>
2025-05-20 22:32:29 +0900

    polymorphism ex2

--- src/poly/car0/CarMain0.java
+++ src/poly/car0/CarMain0.java
@@ -6,5 +6,12 @@ public class CarMain0 {
         K3Car k3Car = new K3Car();
         driver.setCar(k3Car);
         driver.drive();
+
+        Model3Car model3Car = new Model3Car();
+        driver.setCar(null);
+        driver.setModel3Car(model3Car);
+        driver.drive();
+
+
     }
 }
