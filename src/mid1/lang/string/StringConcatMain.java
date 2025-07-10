46652e4076dab60074a6ee95ffbb9ecbcaa595c1
Jukko96 <cslzh4433@gmail.com>
2025-07-10 19:18:34 +0900

    String object concat

---
+++ src/mid1/lang/string/StringConcatMain.java
@@ -0,0 +1,11 @@
+package mid1.lang.string;
+
+public class StringConcatMain {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = "java!";
+        String result = a.concat(b);
+        System.out.println("a+b = "+ a+b );
+        System.out.println("result= "+ result);
+    }
+}
