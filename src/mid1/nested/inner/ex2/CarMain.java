a33894a5be428c2a34a55428d67da988012682c9
Jukko96 <cslzh4433@gmail.com>
2025-11-18 19:51:04 +0900

    add car inner class & refactor car inner class

---
+++ src/mid1/nested/inner/ex2/CarMain.java
@@ -0,0 +1,10 @@
+package mid1.nested.inner.ex2;
+
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car myCar = new Car("model x", 50);
+        myCar.start();
+
+    }
+}
