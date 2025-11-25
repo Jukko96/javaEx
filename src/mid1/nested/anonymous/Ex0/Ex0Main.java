6aa839ef003f5ca241ab6ceb7b3913e31cabb418
Jukko96 <cslzh4433@gmail.com>
2025-11-25 20:00:19 +0900

    anonymous Ex0

---
+++ src/mid1/nested/anonymous/Ex0/Ex0Main.java
@@ -0,0 +1,19 @@
+package mid1.nested.anonymous.Ex0;
+
+public class Ex0Main {
+    public static void HelloJava(){
+        System.out.println("프로그램 시작");
+        System.out.println("Hello Java");
+        System.out.println("프로그램 종료");
+    }
+    public static void HelloSpring(){
+        System.out.println("프로그램 시작");
+        System.out.println("Hello Spring");
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        HelloJava();
+        HelloSpring();
+    }
+}
---
+++ src/mid1/nested/anonymous/Ex0/Ex0RefMain.java
@@ -0,0 +1,17 @@
+package mid1.nested.anonymous.Ex0;
+
+public class Ex0RefMain {
+
+
+    public static void Hello(String stack){
+        System.out.println("프로그램 시작");
+        System.out.println("Hello "+stack);
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        Hello("Java");
+        Hello("Spring");
+    }
+
+}
