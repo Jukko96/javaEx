11ce435d92a9b1f718ce392063bfe83ef9e2f981
Jukko96 <cslzh4433@gmail.com>
2025-12-12 20:04:26 +0900

    exception ex 1

---
+++ src/mid1/exception/ex1/NetworkServiceV1_1.java
@@ -0,0 +1,14 @@
+package mid1.exception.ex1;
+
+
+public class NetworkServiceV1_1 {
+    public void sendMessage (String message){
+        String address = "http://example.com";
+        NetworkClientV1 client = new NetworkClientV1(address);
+
+        client.initError(message);//추가
+        client.connect();
+        client.send(message);
+        client.disconnect();
+    }
+}
