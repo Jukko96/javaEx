26d935c2a9a2d327ea55f996453ada662e3065e8
Jukko96 <cslzh4433@gmail.com>
2026-03-10 20:19:12 +0900

    generic method2

---
+++ src/mid2/generic/ex4/ComplexBox.java
@@ -0,0 +1,17 @@
+package mid2.generic.ex4;
+
+import mid2.generic.animal.Animal;
+
+public class ComplexBox <T extends Animal>{
+    private T animal;
+    public void set(T animal){
+        this.animal = animal;
+    }
+
+    public <T> T printAndReturn (T t){
+        System.out.println("animal.className : "+ animal.getClass().getName());
+        System.out.println("t.className : "+t.getClass().getName());
+        return t;
+
+    }
+}
